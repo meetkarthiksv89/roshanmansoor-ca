992a979a65d252f3c69d0e126d24e2cb0367636d
Melih <melih193@gmail.com>
2019-02-21 21:07:25 +0100

    navbar items padding fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,19 +22,19 @@ render() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Link className="navbar-item py-3" to="/">
+            <Link className="navbar-item py-4" to="/">
               HOME
             </Link>
-            <Link className="navbar-item py-3" to="/about">
+            <Link className="navbar-item py-4" to="/about">
               ABOUT US
             </Link>
-            <Link className="navbar-item py-3" to="/services">
+            <Link className="navbar-item py-4" to="/services">
               SERVICES
             </Link>
-            <Link className="navbar-item py-3" to="/industries">
+            <Link className="navbar-item py-4" to="/industries">
               INDUSTRIES
             </Link>
-            <Link className="navbar-item py-3" to="/career">
+            <Link className="navbar-item py-4" to="/career">
               CAREERS
             </Link>
             <Link className="navbar-item py-3" to="/contact">
