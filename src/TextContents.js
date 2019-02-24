da092cd183cb6f0093dd20b5b930e6ac7030bbe2
Melih <melih193@gmail.com>
2019-02-24 22:11:17 +0100

    index page texts added

---
+++ src/TextContents.js
@@ -0,0 +1,24 @@
+export default {
+  "homepage":{
+    "services_card_title_1":"title",
+    "services_card_text_1":"Some quick example text to build on the card title and make up the bulk of the card's content.",
+    "services_card_title_2":"title",
+    "services_card_text_2":"Some quick example text to build on the card title and make up the bulk of the card's content.",
+    "services_card_title_3":"title",
+    "services_card_text_3":"Some quick example text to build on the card title and make up the bulk of the card's content.",
+    "industries_card_title_1":"INFORMATION TECHNOLOGY",
+    "industries_card_text_1":"Information Technology is the most dynamic industry of today with constant transformations, growth, mergers and acquisi- tions. Our expert team caters to this industry by keeping in mind the constantly changing dynam- ics of the companies operating in this vertical....",
+    "industries_card_title_2":"REAL ESTATE ENGINEERING & CONSTRUCTION",
+    "industries_card_text_2":"Engineering and construction companies have a need for on-going audits to ascertain all aspects of internal operations meet the standards set by the statutory bodies. We understand this and have dedicated re- sources catering to the needs of this industry...",
+    "industries_card_title_3":"MANUFATURING",
+    "industries_card_text_3":"We undertake financial manage- ment for business operating in diverse industries. Our goal is to provide effective solutions that enable our clients to to accom- plish their objectives...",
+    "industries_card_title_4":"RETAIL",
+    "industries_card_text_4":"Retail industry has gone through enormous changes after E-Com- merce disrupted the industry as a whole, which enabled as to in- troduce better ERP system with our clients which results in better accounting system, financial and tax planning and better management of cash flow",
+    "about_us": "ABOUT US",
+    "about_us_title": "A little history",
+    "about_us_text1": "We expect the shipment of a limited quantity of green beans next Monday. We’ll be offering the roasted beans from Tuesday, but quantities are limited, so be quick.",
+    "about_us_text2": "A little history is the highest mountain in Jamaica and one of the highest peaks in the Caribbean at 7,402 ft. It is the home of",
+    "about_us_text3": "Blue Mountain Peak is the highest mountain in Jamaica and one of the highest peaks in the Caribbean at 7,402 ft. It is the home of Blue Mountain coffee and their famous tours. It is located on the border of the Portland and Saint Thomas parishes of Jamaica.",
+    "insights_header": "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a"
+  }
+}
