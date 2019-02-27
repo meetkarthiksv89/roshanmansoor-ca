31b1c7e3fbb0e3b2cf0eb81bbc8ea47a93167810
Melih <melih193@gmail.com>
2019-02-27 23:47:35 +0100

    footer design fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,17 +10,17 @@ export default () => (
           <Col className="col-12 col-sm-4 mb-5 mb-sm-0">
             <SocialMediaImages/>
           </Col>
-          <Col className="col-12 col-sm-3 pl-sm-2 pl-md-3 pl-lg-5 pb-5 pb-sm-0 footer-links footer-text">
-            <Link to="/"><h5>HOME</h5></Link>
-            <Link to="/about"><h5>ABOUT</h5></Link>
-            <Link to="/services"><h5>SERVICES</h5></Link>
-            <Link to="/career"><h5>CAREERS</h5></Link>
-            <Link to="/blog"><h5>BLOG</h5></Link>
-            <Link to="/contact"><h5>CONTACT US</h5></Link>
+          <Col className="col-12 col-sm-4 pl-sm-2 pl-md-3 pl-lg-5 pb-5 pb-sm-0 footer-links footer-text">
+            <Link to="/"><h6 className="ml-sm-3 mb-sm-4 font-weight-bold" >HOME</h6></Link>
+            <Link to="/about"><h6 className="ml-sm-3 mb-sm-4 font-weight-bold" >ABOUT</h6></Link>
+            <Link to="/services"><h6 className="ml-sm-3 mb-sm-4 font-weight-bold" >SERVICES</h6></Link>
+            <Link to="/career"><h6 className="ml-sm-3 mb-sm-4 font-weight-bold" >CAREERS</h6></Link>
+            <Link to="/blog"><h6 className="ml-sm-3 mb-sm-4 font-weight-bold" >BLOG</h6></Link>
+            <Link to="/contact"><h6 className="ml-sm-3 font-weight-bold" >CONTACT US</h6></Link>
           </Col>
-          <Col className="col-12 col-sm-5 pl-sm-2 pl-md-3 pl-lg-5 pb-5 pb-sm-0 footer-text">
-            <h5 className="mb-3 font-weight-bold">LOCATE US</h5>
-            <h6>
+          <Col className="col-12 col-sm-4 pl-sm-2 pl-md-3 pl-lg-5 pb-5 pb-sm-0 footer-text">
+            <h5 className="mb-4 font-weight-bold ml-sm-3">LOCATE US</h5>
+            <h6 className="ml-sm-3">
               Roshan Mansoor & Co.,
               <br/>
               Chartered Accountants,
@@ -30,8 +30,8 @@ export default () => (
               JC Road cross, Bangalore – 560 027
             </h6>
             <br/>
-            <h5 className="mb-3">REACH US</h5>
-            <h6 className="font-weight-bold">
+            <h5 className="mb-4 ml-sm-3">REACH US</h5>
+            <h6 className="font-weight-bold ml-sm-3">
             080-41231220 / 22221220 
             <br/> 
             rm@roshanmansoor.com
