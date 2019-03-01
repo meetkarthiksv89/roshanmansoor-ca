bdef5829be2d695142a85aa37b648e3880e499fc
Melih <melih193@gmail.com>
2019-03-02 00:45:18 +0100

    footer images fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,10 +7,10 @@ export default () => (
   <div className="bg-footer p-5 text-light">
     <Container fluid={true} className="" >
         <Row>
-          <Col className="col-12 col-sm-4 mb-5 mb-sm-0">
+          <Col xs={12} sm={4} className="mb-5 mb-sm-0">
             <SocialMediaImages/>
           </Col>
-          <Col className="col-12 col-sm-4 pl-sm-2 pl-md-3 pl-lg-5 pb-5 pb-sm-0 footer-links footer-text">
+          <Col xs={12} sm={4} className="pl-sm-2 pl-md-3 pl-lg-5 pb-5 pb-sm-0 footer-links footer-text">
             <Link to="/"><h6 className="ml-sm-3 mb-sm-4 font-weight-bold" >HOME</h6></Link>
             <Link to="/about"><h6 className="ml-sm-3 mb-sm-4 font-weight-bold" >ABOUT</h6></Link>
             <Link to="/services"><h6 className="ml-sm-3 mb-sm-4 font-weight-bold" >SERVICES</h6></Link>
@@ -18,7 +18,8 @@ export default () => (
             <Link to="/blog"><h6 className="ml-sm-3 mb-sm-4 font-weight-bold" >BLOG</h6></Link>
             <Link to="/contact"><h6 className="ml-sm-3 font-weight-bold" >CONTACT US</h6></Link>
           </Col>
-          <Col className="col-12 col-sm-4 pl-sm-2 pl-md-3 pl-lg-5 pb-5 pb-sm-0 footer-text">
+          <Col xs={12} sm={4} className="pl-sm-2 pl-md-3 pl-lg-5 pb-5 pb-sm-0 footer-text">
+            <div className="border-bottom hidden-sm-up mb-5"></div>
             <h5 className="mb-4 font-weight-bold ml-sm-3">LOCATE US</h5>
             <h6 className="ml-sm-3">
               Roshan Mansoor & Co.,
@@ -29,6 +30,7 @@ export default () => (
               <br/>
               JC Road cross, Bangalore – 560 027
             </h6>
+            <div className="border-bottom hidden-sm-up mt-4"></div>
             <br/>
             <h5 className="mb-4 ml-sm-3">REACH US</h5>
             <h6 className="font-weight-bold ml-sm-3">
--- src/components/SocialMediaImages.js
+++ src/components/SocialMediaImages.js
@@ -6,7 +6,7 @@ import {Row, Col} from 'react-bootstrap';
 
 export default () => (
   <Row>
-    <Col>
+    <Col className="hidden-sm-down">
     <img
       src={facebook}
       height="60"
@@ -15,17 +15,44 @@ export default () => (
       alt="Facebook"
     />
       <img
-      src={twitter}
-      height="60"
-      width="60"
-      className="d-inline-block align-top mx-3 mr-4"
-      alt="Twitter"
-    />
+        src={twitter}
+        height="60"
+        width="60"
+        className="d-inline-block align-top mx-3 mr-4"
+        alt="Twitter"
+      />
       <img
       src={linkedin}
       height="60"
       width="60"
       className="d-inline-block align-top mx-3 mr-4"
+      alt="Linkedin"
+    />
+    </Col>
+    <Col className="hidden-sm-up">
+      <img
+        src={facebook}
+        height="60"
+        width="60"
+        className="d-inline-block align-top mr-4"
+        alt="Facebook"
+      />
+    </Col>
+    <Col className="hidden-sm-up">
+      <img
+        src={twitter}
+        height="60"
+        width="60"
+        className="d-inline-block align-top mr-4"
+        alt="Twitter"
+      />
+    </Col>
+    <Col className="hidden-sm-up">
+    <img
+      src={linkedin}
+      height="60"
+      width="60"
+      className="d-inline-block align-top mr-4"
       alt="Linkedin"
     />
     </Col>
