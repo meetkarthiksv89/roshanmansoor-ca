4f095fd12e8ff885489d86f989b3541c01cd3e9f
Melih <melih193@gmail.com>
2019-03-03 17:09:59 +0100

    footer social icons moved to center

--- src/pages/services/index.js
+++ src/pages/services/index.js
@@ -72,12 +72,12 @@ export const ServicesPage = () => {
           <h6 className="font-weight-bold" style={{"width":"150px"}}>{TextContents.services_index.taxation_opt1}  <span className="float-right"> <OrangeArrowComp width="18px"/></span></h6>
           <h6 className="font-weight-bold" style={{"width":"150px"}}>{TextContents.services_index.taxation_opt2}  <span className="float-right"> <OrangeArrowComp width="18px"/></span></h6>
         </Col>
-        <Col xs={12} sm={4} className="p-0 col-md-4 ">
+        <Col xs={12} sm={4} className="p-0">
           <img src="https://via.placeholder.com/500x420" alt=""/>
         </Col>
       </Row>
       <Row className="mx-4 bg-white mt-5 hidden-sm-up">
-        <Col xs={12} sm={4} className="p-0 col-md-4 ">
+        <Col xs={12} sm={4} className="p-0">
           <img src="https://via.placeholder.com/500x420" alt=""/>
         </Col>
         <Col xs={12} sm={5} className="py-5 px-3 pr-sm-0 pl-sm-5 pr-0 service-link">
@@ -118,8 +118,8 @@ export const ServicesPage = () => {
           <h6 className="font-weight-bold" style={{"width":"220px"}}>{TextContents.services_index.advisory_opt4} <span className="float-right"> <OrangeArrowComp width="18px"/></span></h6>
         </Col>
       </Row>
-      <Row className="mx-4 bg-white mt-5 hidden-sm-down">        
-        <Col xs={12} sm={3} className="py-5 px-3 pr-sm-0 pl-sm-5 pr-0 service-link">
+      <Row className="mx-4 bg-white mt-5 hidden-sm-down">
+        <Col xs={12} sm={5} className="pl-3 pl-sm-5 py-5 service-link">
           <Link to="services/otherservices">
             <h4 className="font-weight-bold">
               {TextContents.services_index.other_services}
@@ -131,15 +131,14 @@ export const ServicesPage = () => {
             <h6 className="font-weight-bold" style={{"width":"300px"}}>{TextContents.services_index.other_services_opt3} <span className="float-right"> <OrangeArrowComp width="18px"/></span></h6>
           </Link>
         </Col>
-        <Col xs={12} sm={5} className="py-sm-5 py-3 px-sm-5 px-3 mt-0 mt-sm-5">
-          
+        <Col xs={12} sm={3} className="py-5 px-sm-0 px-3 mt-0 mt-sm-5">
         </Col>
-        <Col xs={12} sm={4} className="p-0 col-md-4 ">
+        <Col xs={12} sm={4} className="p-0 col-md-4">
           <img src="https://via.placeholder.com/500x420" alt=""/>
         </Col>
       </Row>
       <Row className="mx-4 bg-white mt-5 hidden-sm-up">  
-        <Col xs={12} sm={4} className="p-0 col-md-4 ">
+        <Col xs={12} sm={4} className="p-0">
           <img src="https://via.placeholder.com/500x420" alt=""/>
         </Col>      
         <Col xs={12} sm={3} className="py-5 px-3 pr-sm-0 pl-sm-5 pr-0 service-link">
