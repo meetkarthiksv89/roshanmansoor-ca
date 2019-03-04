b062262c666d71353aebfe585f3ee98a69c4d8f6
Melih <melih193@gmail.com>
2019-03-04 01:33:06 +0100

    slider image size increased for mobile devices

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,37 +6,60 @@ const Slider = () => {
     <Carousel
       controls={false}>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/1000x450"
-          srcSet="https://via.placeholder.com/400x450 568w, https://via.placeholder.com/700x450 780w , https://via.placeholder.com/1000x450 1000w"
-          alt="First slide"
-        />
+        <div className="hidden-sm-down">
+          <img
+            className="d-block w-100"
+            src="https://via.placeholder.com/1000x450"
+            alt="First slide"
+          />
+        </div>
+        <div className="hidden-sm-up">
+          <img
+            className="d-block w-100"
+            src="https://via.placeholder.com/400x450"
+            alt="First slide"
+          />
+        </div>
         <Carousel.Caption className="custom-carousel-caption">
           <h1>First slide label</h1>
           <h3>Nulla vitae elit libero, a pharetra augue mollis interdum.</h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/1000x450"
-          srcSet="https://via.placeholder.com/400x450 568w, https://via.placeholder.com/700x450 780w , https://via.placeholder.com/1000x450 1000w"
-          alt="Second slide"
-        />
+        <div className="hidden-sm-down">
+          <img
+            className="d-block w-100"
+            src="https://via.placeholder.com/1000x450"
+            alt="Second slide"
+          />
+        </div>
+        <div className="hidden-sm-up">
+          <img
+            className="d-block w-100"
+            src="https://via.placeholder.com/400x450"
+            alt="Second slide"
+          />
+        </div>
         <Carousel.Caption className="custom-carousel-caption">
           <h1>Second slide label</h1>
           <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://via.placeholder.com/1000x450"
-          srcSet="https://via.placeholder.com/400x450 568w, https://via.placeholder.com/700x450 780w , https://via.placeholder.com/1000x450 1000w"
-          alt="Third slide"
-        />
-
+        <div className="hidden-sm-down">
+          <img
+            className="d-block w-100"
+            src="https://via.placeholder.com/1000x450"
+            alt="Third slide"
+          />
+        </div>
+        <div className="hidden-sm-up">
+          <img
+            className="d-block w-100"
+            src="https://via.placeholder.com/400x450"
+            alt="Third slide"
+          />
+        </div>
         <Carousel.Caption className="custom-carousel-caption">
           <h1>Third slide label</h1>
           <h3>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</h3>
