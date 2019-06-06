db99a2a02ca81686d4534192c9aac869ed03b022
Melih <melih193@gmail.com>
2019-06-06 10:04:11 +0200

    comment blog section

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -5,34 +5,37 @@ import {Container, Row, Col, Card} from 'react-bootstrap'
 
 export default class BlogPage extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
-
+    // const { data } = this.props
+    // const { edges: posts } = data.allMarkdownRemark
     return (
-      <Layout>
-        <Container>
-          <Row className="mb-5 mt-3">
-          {posts
-            .map(({ node: post }) => (
-              <Col xs={6} sm={4} key={post.id}>
-                <Card className="bg-dark box-shadow-none">
-                  <Link to={post.fields.slug}>
-                    <Card.Body style={{minHeight:"450px"}}className="my-4 py-4">
-                      <Card.Title>{post.frontmatter.title} {post.frontmatter.date}</Card.Title>
-                      <Card.Text>
-                      {post.excerpt}
-                        <br/>
-                        <Link to={post.fields.slug} className="text-orange font-weight-bold px-sm-3 py-1 float-right">Read More </Link>
-                      </Card.Text>
-                    </Card.Body>
-                  </Link>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </Container>
-      </Layout>
-    )
+      <>
+      </>
+    );
+    // return (
+    //   <Layout>
+    //     <Container>
+    //       <Row className="mb-5 mt-3">
+    //       {posts
+    //         .map(({ node: post }) => (
+    //           <Col xs={6} sm={4} key={post.id}>
+    //             <Card className="bg-dark box-shadow-none">
+    //               <Link to={post.fields.slug}>
+    //                 <Card.Body style={{minHeight:"450px"}}className="my-4 py-4">
+    //                   <Card.Title>{post.frontmatter.title} {post.frontmatter.date}</Card.Title>
+    //                   <Card.Text>
+    //                   {post.excerpt}
+    //                     <br/>
+    //                     <Link to={post.fields.slug} className="text-orange font-weight-bold px-sm-3 py-1 float-right">Read More </Link>
+    //                   </Card.Text>
+    //                 </Card.Body>
+    //               </Link>
+    //             </Card>
+    //           </Col>
+    //         ))}
+    //       </Row>
+    //     </Container>
+    //   </Layout>
+    // )
   }
 }
 
